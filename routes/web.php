93f1ce572daf514cf46526326222ff58f10da556
FectyFecty <azwanrazan@gmail.com>
2025-11-06 01:14:40 +0700

    Add Calculator Routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KalkulatorController;
 
-Route::get('/', function () {
-    return view('dashboard');
-});
-
+// Your final routes
+Route::get('/', [KalkulatorController::class, 'dashboard']);
+Route::get('/dashboard', [KalkulatorController::class, 'dashboard'])->name('dashboard');
+Route::post('/calculate', [KalkulatorController::class, 'calculate'])->name('calculate');
+Route::delete('/clear-history/{id}', [KalkulatorController::class, 'clearHistory'])->name('clear-history');
