5036dfaeb414cfb8aef1fc5c3100a9c674c0f888
s0nchae <noval.dwiputra@gmail.com>
2025-12-04 12:19:14 +0700

    Carousel dan Dropdown fixed

--- resources/views/layout/partials/script.blade.php
+++ resources/views/layout/partials/script.blade.php
@@ -1,6 +1,7 @@
 <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 <script>
 function allowDrop(ev) {
